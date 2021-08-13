46e5ef23c421442ceb45027713089113daa191d7
KawsarRobin <kawsrrobin3@gmail.com>
2021-08-14 02:05:49 +0600

    js added

---
+++ extra.js
@@ -0,0 +1,7 @@
+// ১. নতুন একটা স্ক্রিপ্ট ফাইল খুলবে। সেটাকে index.html এর সাথে কানেক্ট করতে পারো কিনা?
+console.log("hello");
+
+// ২. যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা।
+
+const h2 = document.getElementsByTagName("h2");
+console.log(h2);
